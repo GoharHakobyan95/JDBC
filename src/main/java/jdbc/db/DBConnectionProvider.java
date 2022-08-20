6d44078d1ff02f7a395c1dd84eb7934c3b13e4f2
Gohar Hakobyan <hak.goh95@gmail.com>
2022-08-20 14:18:47 +0400

    JDBC example

--- src/main/java/jdbc/db/DBConnectionProvider.java
+++ src/main/java/jdbc/db/DBConnectionProvider.java
@@ -8,7 +8,7 @@ public class DBConnectionProvider {
     private static DBConnectionProvider instance = new DBConnectionProvider();
 
     private  Connection connection;
-    private static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
+    private static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
     private static final String DB_URL = "jdbc:mysql://localhost:3306/user?characterEncoding=utf8";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
